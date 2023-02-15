337e880496fa67815751434e542608a51cf5a4c3
larger0o <largeroliker0o@gmail.com>
2023-02-15 18:26:44 +0100

    Initial commit

---
+++ src/test/java/es/aclavijo/bike/BikeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.aclavijo.bike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
